ca282412b32f3b01a8859c42213fe17d8fe4669a
Youssef Nabeh <yousseffe.nabeh1@gmail.com>
2024-09-05 22:10:37 +0300

    Add project files.

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.BLL.Interface;
+
+namespace MVC_Project.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _repository;
+        public DepartmentController(IDepartmentRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            var departments = _repository.GetAll();
+            return View(departments);
+        }
+    }
+}
+
